26c0104d38893353530126978026e9ca891f8c87
Shashank Kumar Gupta <91917475+shashank-2310@users.noreply.github.com>
2024-06-12 13:58:11 +0530

    graph fixed

--- components/PercentileLineChart.tsx
+++ components/PercentileLineChart.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useSearchParams } from 'next/navigation';
 import { LineChart, Line, XAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 const PercentileLineChart = () => {
 
     const searchParams = useSearchParams();
@@ -10,18 +11,25 @@ const PercentileLineChart = () => {
         { name: 0, uv: 10 },
         { name: 25, uv: 12 },
         { name: 50, uv: 5 },
-        { name: 75, uv: 2 },
-        { name: 100, uv: 8 },
-        { name: percentile, uv: 1 }
+        { name: 75, uv: 4 },
+        { name: 100, uv: 2 },
     ];
 
-    const sortedData = [...data].sort((a, b) => a.name - b.name);
+    const updatedData = [
+        ...(data.some((item: { name: number }) => item.name === percentile) ? data.map((item: { name: number, uv: number }) => {
+            if (item.name === percentile) {
+                return { ...item, uv: item.uv + 1 };
+            }
+            return item;
+        }) : [{ name: percentile, uv: 1 }])
+    ];
+    const sortedData = [...data, ...updatedData].sort((a, b) => a.name - b.name);
 
     return (
         <ResponsiveContainer width="100%" height={300}>
             <LineChart width={500} height={300} data={sortedData} margin={{ top: 5, right: 20, bottom: 5, left: 20 }}>
                 <Line name='numberOfStudents' type="monotone" dataKey="uv" stroke="#8884d8" />
-                <XAxis dataKey="name" />
+                <XAxis dataKey="name" allowDuplicatedCategory={false} />
                 <Legend />
                 <Tooltip />
             </LineChart>
